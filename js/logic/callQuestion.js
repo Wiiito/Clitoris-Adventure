f9fcedae1b03dc6b68ad0dbf1d037519c09630a2
Wiito <tminemcraft123@gmail.com>
2022-11-18 19:58:44 -0300

    Avermelhando o vermelho avermelhado

--- js/logic/callQuestion.js
+++ js/logic/callQuestion.js
@@ -63,7 +63,7 @@ async function answer(e) {
     e.style.backgroundColor = '#00b339'
     rightAnswer()
   } else {
-    e.style.backgroundColor = '#c4221f'
+    e.style.backgroundColor = '#ed1915'
     wrongAnswer()
   }
   setTimeout(() => {
